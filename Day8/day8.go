a85143d7a3ffb7d3c9b7da5f2762926dec6c2ffb
j.r <j.r@jugendhacker.de>
2020-12-08 10:27:30 +0100

    Add Day8 challenge 2 with unit tests

--- Day8/day8.go
+++ Day8/day8.go
@@ -20,7 +20,13 @@ func Run(input string) {
 
 	scanner := bufio.NewScanner(f)
 	instructions := parseData(scanner)
+	instrctionsCopy := make(map[int]instruction)
+	for i, inst := range instructions {
+		instrctionsCopy[i] = inst
+	}
 	fmt.Printf("Challenge 1: accumulator value is %v\n", challenge1(instructions, 0, 0))
+	_, accumulator := challenge2(instrctionsCopy, 0, false)
+	fmt.Printf("Challenge 2: accumulator value is %v\n", accumulator)
 }
 
 func parseData(input *bufio.Scanner) (instructions map[int]instruction) {
@@ -56,5 +62,37 @@ func challenge1(instructions map[int]instruction, index int, accumulator int) (a
 	case "jmp":
 		return challenge1(instructions, index+instructions[index].value, accumulator)
 	}
-	return 0
+	return accumulator
+}
+
+func challenge2(instructions map[int]instruction, index int, changed bool) (noLoop bool, accumulator int) {
+	if instructions[index].ran {
+		return false, 0
+	} else if instructions[index].command == "" {
+		return true, 0
+	}
+	inst := instructions[index]
+	inst.ran = true
+	instructions[index] = inst
+	switch instructions[index].command {
+	case "acc":
+		noLoop, accumulator = challenge2(instructions, index+1, changed)
+		accumulator += instructions[index].value
+		return noLoop, accumulator
+	case "nop":
+		noLoop, accumulator = challenge2(instructions, index+1, changed)
+		if !noLoop && !changed {
+			changed = true
+			noLoop, accumulator = challenge2(instructions, index+instructions[index].value, changed)
+		}
+		return noLoop, accumulator
+	case "jmp":
+		noLoop, accumulator = challenge2(instructions, index+instructions[index].value, changed)
+		if !noLoop && !changed {
+			changed = true
+			noLoop, accumulator = challenge2(instructions, index+1, changed)
+		}
+		return noLoop, accumulator
+	}
+	return false, 0
 }
--- Day8/day8_test.go
+++ Day8/day8_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-const challenge1Data = `nop +0
+const challengeData = `nop +0
 acc +1
 jmp +4
 acc +3
@@ -17,10 +17,19 @@ jmp -4
 acc +6`
 
 func TestChallenge1(t *testing.T) {
-	scanner := bufio.NewScanner(strings.NewReader(challenge1Data))
+	scanner := bufio.NewScanner(strings.NewReader(challengeData))
 	instructions := parseData(scanner)
 	result := challenge1(instructions, 0, 0)
 	if result != 5 {
 		t.Errorf("Challenge 1: wrong result %v, wanted %v", result, 5)
+	}
+}
+
+func TestChallenge2(t *testing.T) {
+	scanner := bufio.NewScanner(strings.NewReader(challengeData))
+	instructions := parseData(scanner)
+	_, result := challenge2(instructions, 0, false)
+	if result != 8 {
+		t.Errorf("Challenge 2: wrong result %v, wanted %v", result, 8)
 	}
 }
