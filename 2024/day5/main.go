facd549c9a3e13720f917dcc24707040b4810ace
j.r <j.r@jugendhacker.de>
2024-12-05 10:05:01 +0100

    Day 5 part 2
    Probably need to clean up a bit later today

--- 2024/day5/main.go
+++ 2024/day5/main.go
@@ -18,6 +18,7 @@ func main() {
 	rules := make(map[int][]int)
 
 	sum := 0
+	part2_sum := 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		if !rules_finished {
@@ -28,11 +29,7 @@ func main() {
 			line_split := strings.Split(line, "|")
 			rule_l, _ := strconv.Atoi(line_split[0])
 			rule_r, _ := strconv.Atoi(line_split[1])
-			if _, ok := rules[rule_l]; ok {
-				rules[rule_l] = append(rules[rule_l], rule_r)
-			} else {
-				rules[rule_l] = []int{rule_r}
-			}
+			rules[rule_l] = append(rules[rule_l], rule_r)
 			continue
 		} else {
 			line_split := strings.Split(line, ",")
@@ -54,10 +51,25 @@ func main() {
 				mid := int(len(line_split) / 2)
 				mid_e, _ := strconv.Atoi(line_split[mid])
 				sum += mid_e
+			} else {
+				line_nums := make([]int, len(line_split))
+				for i, n := range line_split {
+					line_nums[i], _ = strconv.Atoi(n)
+				}
+				slices.SortStableFunc(line_nums, func(a int, b int) int {
+					if slices.Contains(rules[a], b) {
+						return -1
+					} else if slices.Contains(rules[b], a) {
+						return 1
+					}
+					return 0
+				})
+				mid := int(len(line_nums) / 2)
+				part2_sum += line_nums[mid]
 			}
 		}
 
 	}
 
-	fmt.Println(sum)
+	fmt.Println(part2_sum)
 }
